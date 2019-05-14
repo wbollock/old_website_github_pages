0a5f101ff9086be0d626c4950864b7d73e6e9189
Will Bollock <wbollock@gmail.com>
2019-05-14 15:13:46 -0400

    jquery init

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,18 @@ $('#mode').change(function(){
 // display last modified date
 var lm = document.lastModified;
 document.getElementById("lastModified").innerHTML = lm;
+
+//sticky nav bar
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
