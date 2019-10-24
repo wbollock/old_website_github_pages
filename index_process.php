26c2e4d5ba9924587fcf39bbef1642086b44a853
Will Bollock <wbollock@gmail.com>
2019-10-24 15:00:38 -0400

    form init

---
+++ index_process.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <title>Will Bollock's Website</title>
+</head>
+<body>
+        <p>You entered <?php$_GET["code1"];?> for DOCTYPE</p>
+</body>
+</html>
